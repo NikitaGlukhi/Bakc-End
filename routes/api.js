6f6dba0c855dbddf0f2b4938e9177b256841d9fb
NikitaGlukhi <nikita.glukhi@mail.ru>
2018-03-20 18:45:31 +0200

    Little changes

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('post_office_db', 'root', 'Nikita06021999', {
+const sequelize = new Sequelize('post_office_db', 'root', 'password', {
     host: "localhost",
     dialect: "mysql",
 
